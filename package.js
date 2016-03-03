fddebe99daabd0a02ef6c2228f0152f8803bfdc2
Max Bruchmann <maxbruchmann@gmail.com>
2016-03-03 17:54:03 +0100

    2.2.0

--- package.js
+++ package.js
@@ -1,29 +1,43 @@
 Package.describe({
   summary: "Material design icons from google",
   name:"planettraining:material-design-icons",
-  version: "2.1.1",
+  version: "2.2.0",
   git:"https://github.com/planet-training/material-design-icons.git"
 });
 
 Package.onUse(function (api) {
   api.versionsFrom('1.0');
   api.addAssets([
-    /* icon sprites */
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-alert.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action-symbol.svg',
     'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-av.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg',
     'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-device.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-editor.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-file.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-hardware.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-hardware-symbol.svg',
     'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-maps.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-notification.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-places-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-action.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-editor-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-hardware.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation-symbol.svg',
     'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-places.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-alert-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-communication.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-editor.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-navigation.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-alert.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-file-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-image.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-notification-symbol.svg',
     'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-social.svg',
-    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle.svg'
-  ], "client");
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-av-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-content.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-file.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-maps-symbol.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-notification.svg',
+    'bower_components/material-design-icons/sprites/svg-sprite/svg-sprite-toggle-symbol.svg'
+], "client");
 });
